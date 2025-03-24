97f612868063daddd358b593d6c6414e5376e0a4
anthonyargatoff <anthonyargatoff@gmail.com>
2025-03-24 16:35:24 -0700

    Add complete game scene

---
+++ Assets/Scripts/EndScreenManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreenManager : MonoBehaviour
+{
+
+  public void MainMenu()
+  {
+    SceneManager.LoadScene("MainMenu");
+  }
+}
