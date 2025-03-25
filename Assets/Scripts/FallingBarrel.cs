27886fdf433e462470378962ca6aa591dcfa112f
anthonyargatoff <anthonyargatoff@gmail.com>
2025-03-24 17:29:49 -0700

    Fix falling barrel collisions

--- Assets/Scripts/FallingBarrel.cs
+++ Assets/Scripts/FallingBarrel.cs
@@ -4,9 +4,11 @@ public class FallingBarrel : MonoBehaviour
 {
   private Rigidbody2D barrelRigidBody;
   [SerializeField] float barrelFallSpeed;
+  [SerializeField] Barrel rollingBarrel;
   void Start()
   {
     barrelRigidBody = GetComponent<Rigidbody2D>();
+    Physics2D.IgnoreCollision(rollingBarrel.GetComponent<Collider2D>(), GetComponent<Collider2D>());
   }
 
   // Update is called once per frame
