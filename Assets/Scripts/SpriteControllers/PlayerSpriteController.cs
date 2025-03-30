70f5ee59698617dd60b0c2d2b01ef1699693248f
Alexander <alexargatoff@gmail.com>
2025-03-30 11:37:09 -0700

    removed hammer move and ladder sound

--- Assets/Scripts/SpriteControllers/PlayerSpriteController.cs
+++ Assets/Scripts/SpriteControllers/PlayerSpriteController.cs
@@ -174,14 +174,14 @@ public class PlayerSpriteController: SpriteControllerMonoBehaviour
             walkFrame = controller.IsWalking && !walkFrame;
             if (controller.IsWalking)
             {
-                AudioManager.PlaySound(Audios.Move, volume: 0.5f, pitch: pitches[walkPitchIndex]);
+                AudioManager.PlaySound(Audios.Move, controller.UsingHammer ? 0f : 0.5f, pitch: pitches[walkPitchIndex]);
                 walkPitchIndex++;
                 if (walkPitchIndex >= pitches.Length) walkPitchIndex = 0;
             }
             
             if (controller.IsClimbing && Mathf.Abs(rb.linearVelocityY) > 0.01f)
             {
-                AudioManager.PlaySound(Audios.Ladder, volume: 0.5f, pitch: pitches[climbPitchIndex]);
+                AudioManager.PlaySound(Audios.Ladder, controller.UsingHammer ? 0f: 0.5f, pitch: pitches[climbPitchIndex]);
                 climbPitchIndex++;
                 if (climbPitchIndex >= pitches.Length) climbPitchIndex = 0;
             }
