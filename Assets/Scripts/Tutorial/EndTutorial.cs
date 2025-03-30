ca9fa951c19660d2665c914756ae47d17689be73
bhunt02 <bridgette.hunt02@outlook.com>
2025-03-30 16:25:25 -0700

    slight fix to tutorial progression & player prefab

--- Assets/Scripts/Tutorial/EndTutorial.cs
+++ Assets/Scripts/Tutorial/EndTutorial.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 
 public class EndTutorial : MonoBehaviour
 {
+  private bool triggered;
   void OnTriggerEnter2D(Collider2D collision)
   {
-    if (collision.CompareTag("Player"))
+    if (collision.CompareTag("Player") && !triggered)
     {
+      triggered = true;
       var text = GameObject.Find("EndTutorial").GetComponent<TextMeshProUGUI>();
       text?.SetText("Congratulations, you've completed the tutorial!");
       GameManager.NextTutorial();
--- Assets/Scripts/Tutorial/NextTutorialCheck.cs
+++ Assets/Scripts/Tutorial/NextTutorialCheck.cs
@@ -3,10 +3,12 @@ using UnityEngine.Events;
 
 public class NextTutorialCheck : MonoBehaviour
 {
+  private bool triggered;
   void OnTriggerEnter2D(Collider2D collision)
   {
-    if (collision.CompareTag("Player"))
+    if (collision.CompareTag("Player") && !triggered)
     {
+      triggered = true;
       GameManager.NextTutorial();
     }
   }
