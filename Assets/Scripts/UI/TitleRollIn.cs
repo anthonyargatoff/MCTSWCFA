a0c24c76818989ffbf4ec9e86a1ca48065f841c5
bhunt02 <bridgette.hunt02@outlook.com>
2025-03-30 11:01:37 -0700

    improved title screen

--- Assets/Scripts/UI/TitleRollIn.cs
+++ Assets/Scripts/UI/TitleRollIn.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
@@ -11,7 +12,14 @@ public class TitleRollIn : MonoBehaviour
     private bool spritesLoaded;
     private static List<Sprite> titleSprites = new();
     private static int _numSprites;
+
+    private Transform titleTransform;
     private RectTransform rectTransform;
+    private Image mainBg;
+    private RectTransform bgOverlay;
+    private RectTransform i1;
+    private RectTransform i2;
+    private GameObject buttonCanvas;
     
     private bool titleRolledIn;
     
@@ -27,7 +35,13 @@ public class TitleRollIn : MonoBehaviour
             }
             spritesLoaded = true;
         }
-        rectTransform = GetComponent<RectTransform>();
+        titleTransform = transform.Find("TitleContainer");
+        rectTransform = titleTransform.GetComponent<RectTransform>();
+        mainBg = transform.Find("Background1").GetComponent<Image>();
+        bgOverlay = transform.Find("Background2").GetComponent<RectTransform>();
+        i1 = bgOverlay.Find("I1").GetComponent<RectTransform>();
+        i2 = bgOverlay.Find("I2").GetComponent<RectTransform>();
+        buttonCanvas = GameObject.Find("ButtonCanvas");
         
         StartCoroutine(RollInTitle());
     }
@@ -37,38 +51,60 @@ public class TitleRollIn : MonoBehaviour
         if (titleRolledIn) yield break;
         titleRolledIn = true;
         
+        var buttonContainer = buttonCanvas?.transform.Find("ButtonContainer");
+        if (buttonContainer)
+        {
+            buttonContainer.localScale = new Vector3(0f, 1f, 1f);
+        }
+        
         var sample = titleSprites[0];
-        var ppuMod = 2f;
-        var ppu = sample.pixelsPerUnit / ppuMod;
-        rectTransform.sizeDelta = new Vector2(sample.texture.width * ppuMod, sample.texture.height * ppuMod);
-        var offsetY = -rectTransform.sizeDelta.y / 2;
+        var scale = 1.5f;
+        rectTransform.sizeDelta = new Vector2(sample.texture.width * scale, sample.texture.height * scale);
+        var offsetY = -rectTransform.sizeDelta.y / 2 - 50f;
         rectTransform.anchoredPosition = new Vector2(0, offsetY);
         
-        var offset = new Vector2(sample.texture.width / 2f / ppu, (sample.texture.height + offsetY) / 2f / ppu);
         yield return new WaitForSecondsRealtime(1f);
         for (var i = 0; i < _numSprites; i++)
         {
-            var img = CreateSpriteImage(i, offset, ppu);
+            var img = CreateSpriteImage(i, scale);
             var goalY = img.rectTransform.position.y;
-            img.rectTransform.position= new Vector2(img.rectTransform.position.x, img.rectTransform.position.y + rectTransform.sizeDelta.y / ppu);
+            img.rectTransform.position= new Vector2(img.rectTransform.position.x, img.rectTransform.position.y + rectTransform.sizeDelta.y * scale);
             img.enabled = true;
-            img.rectTransform.DOMoveY(goalY, 1f + Random.value * 0.5f).SetEase(Ease.OutBounce).SetUpdate(true).SetLink(img.gameObject);
+            var time = 1f + Random.value * 0.5f;
+            img.rectTransform.DOMoveY(goalY, time).SetEase(Ease.OutBounce).SetUpdate(true).SetLink(img.gameObject);
         }
+        
+        yield return new WaitForSeconds(0.5f);
+        bgOverlay.DOMoveY(Screen.height/2f, 1f);
+        yield return new WaitForSeconds(1.5f);
+        StartCoroutine(PulseBeast());
+        i1.DOMoveX(i1.position.x - 250f, 1f);
+        i2.DOMoveX(i2.position.x + 250f, 1f);
+        buttonContainer?.DOScaleX(1,1f);
     }
 
-    private Image CreateSpriteImage(int i, Vector2 offset, float ppu)
+    private IEnumerator PulseBeast()
+    {
+        while (!this.IsDestroyed() && enabled)
+        {
+            mainBg.DOColor(new Color(1, 1, 1, mainBg.color.a > 0.25f ? 0.25f : 0.75f), 1f);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+    
+    private Image CreateSpriteImage(int i, float scale)
     {
         var obj = new GameObject();
-        obj.transform.SetParent(transform);
+        obj.transform.SetParent(titleTransform);
         obj.name = titleSprites[i].name;
         
         var img = obj.AddComponent<Image>();
         var sprite = titleSprites[i];
         img.sprite = sprite;
-        img.rectTransform.anchorMin = new Vector2(0, 1);
-        img.rectTransform.anchorMax = new Vector2(0, 1);
-        img.rectTransform.sizeDelta = new Vector2(sprite.rect.width / ppu, sprite.rect.height / ppu);
-        img.rectTransform.position = new Vector2(sprite.rect.center.x / ppu - offset.x, sprite.rect.center.y / ppu - offset.y);
+        img.rectTransform.anchorMin = new Vector2(0, 0);
+        img.rectTransform.anchorMax = new Vector2(0, 0);
+        img.rectTransform.sizeDelta = new Vector2(sprite.rect.width, sprite.rect.height) * scale;
+        img.rectTransform.anchoredPosition = new Vector2(sprite.rect.center.x, sprite.rect.center.y) * scale;
         img.enabled = false;
         
         return img;
